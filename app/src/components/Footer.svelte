6b51a07a530c76f704cb5e86bd59b16b93f28e8a
MohamedElmdary <engm5081@gmail.com>
2023-06-08 14:55:11 +0300

    Add api.json

--- app/src/components/Footer.svelte
+++ app/src/components/Footer.svelte
@@ -1,8 +1,14 @@
-<footer class="bg-primary py-1 fixed-bottom">
+<footer class="bg-body-tertiary py-1 fixed-bottom">
   <div
     class="container text-center text-color-1"
     style="color: var(--bs-gray-200);"
   >
-    Copyright © 2023 MrrTech
+    Copyright © 2023 <a
+      href="https://github.com/mohamedElmdary"
+      target="_blank"
+      class="link-offset-2 link-underline link-underline-opacity-0"
+    >
+      @MohamedElmdary
+    </a>
   </div>
 </footer>
