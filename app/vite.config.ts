7449b33b36cd2285e2e37b0addec1820861e943f
MohamedElmdary <engm5081@gmail.com>
2023-06-08 04:39:13 +0300

    🚀 release v1.0.0

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,8 +4,8 @@ import { svelte } from "@sveltejs/vite-plugin-svelte"
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "dist",
+    outDir: "../",
   },
-  base: "/",
+  base: "/icons/",
   plugins: [svelte()],
 })
